b41e429e3802621a9611e5bba8ade8d4f80a1ae6
ubuntu <TobiasFaller@gmx.de>
2018-04-16 09:28:39 -0700

    added varius blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -13,15 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-import job.views
-
+from django.urls import path
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', job.views.home, name='home'),
-    path('blog/', include('blog.urls')),
+    path('', views.allblogs, name='allblogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
 
-]  +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+]
